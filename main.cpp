e7cf37d70a8372565dae1de13f07d06925a9d934
Raphael Manayon <rapmanayon@gmail.com>
2024-12-22 10:07:50 -0800

    Video 2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 
-consteval int get_version() {
-    return 20;
+unsigned int dogs {};
+
+unsigned int add_student() {
+    static unsigned int students {};
+
+    students++;
+    dogs++;
+    return students;
+}
+
+unsigned int add_dog() {
+    dogs++;
+    return dogs;
 }
 
 int main() {
-    std::cout << "Hello, World in C++" << get_version() << "!" << std::endl;
+    for (size_t i {}; i < 20; i++) {
+        std::cout << "students: " << add_student() << std::endl;
+        std::cout << "dogs: " << add_dog() << std::endl;
+    }
+    
     return 0;
 }
